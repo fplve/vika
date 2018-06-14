9d01cea23c49666e41780128ca7fecff6e8d5292
fplve <fplve@yandex.ru>
2018-06-14 17:53:53 +0600

    0.2

---
+++ build.cmd
@@ -0,0 +1,1 @@
+java -Dusername=user -Dpassword=pass -jar mvn-classloader-1.8.jar  com.github.igor-suhorukov:maven-cli:3.3.9 com.github.igorsuhorukov.maven.MavenRunner clean package scm-publish:publish-scm
